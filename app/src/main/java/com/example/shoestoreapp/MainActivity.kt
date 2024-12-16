c900d0c6256057e543df759bfb8f0856b1eb46f4
22120419 <nguyenvanty147258@gmail.com>
2024-12-16 09:17:02 +0700

    update page intro activity

--- app/src/main/java/com/example/shoestoreapp/MainActivity.kt
+++ app/src/main/java/com/example/shoestoreapp/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.shoestoreapp.activity.IntroActivity
 import com.example.shoestoreapp.activity.NotificationActivity
 import com.example.shoestoreapp.activity.SettingActivity
 import com.google.firebase.database.ktx.database
@@ -15,13 +16,6 @@ import com.google.firebase.ktx.Firebase
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // Write a message to the database
-//        val database = Firebase.database
-//        val myRef = database.getReference("message")
-//
-//        myRef.setValue("Hello, dsf sdfsdf!")
-        val intent = Intent(this, SettingActivity::class.java)
-        startActivity(intent)
 
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
--- app/src/main/java/com/example/shoestoreapp/activity/IntroActivity.kt
+++ app/src/main/java/com/example/shoestoreapp/activity/IntroActivity.kt
@@ -1,6 +1,32 @@
+//package com.example.shoestoreapp.activity
+//
+//import android.os.Bundle
+//import androidx.activity.enableEdgeToEdge
+//import androidx.appcompat.app.AppCompatActivity
+//import androidx.core.view.ViewCompat
+//import androidx.core.view.WindowInsetsCompat
+//import com.example.shoestoreapp.R
+//
+//class IntroActivity : AppCompatActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        enableEdgeToEdge()
+//        setContentView(R.layout.activity_intro)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+//    }
+//}
+
+
 package com.example.shoestoreapp.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,10 +38,24 @@ class IntroActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_intro)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        findViewById<View>(R.id.btnIntro).setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+
+        }
+
+        findViewById<View>(R.id.txtLogin).setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
+
