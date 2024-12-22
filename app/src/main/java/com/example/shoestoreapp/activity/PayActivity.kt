e26f85975d7206860f4c802959a424fb99b88f27
Nghiadz2004 <huunghia122004@gmail.com>
2024-12-22 21:40:23 +0700

    final-commit-18.12

---
+++ app/src/main/java/com/example/shoestoreapp/activity/PayActivity.kt
@@ -0,0 +1,13 @@
+package com.example.shoestoreapp.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.shoestoreapp.R
+
+class PayActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pay)
+
+    }
+}
