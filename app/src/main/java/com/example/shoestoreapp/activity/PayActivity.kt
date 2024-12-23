b1a692e8c2927fbbceae848252a6f498f4ef378c
Nghiadz2004 <huunghia122004@gmail.com>
2024-12-23 15:56:06 +0700

    checkout_UI

--- app/src/main/java/com/example/shoestoreapp/activity/PayActivity.kt
+++ app/src/main/java/com/example/shoestoreapp/activity/PayActivity.kt
@@ -3,11 +3,33 @@ package com.example.shoestoreapp.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.shoestoreapp.R
+import com.example.shoestoreapp.data.model.Product
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class PayActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pay)
 
+        // Nhận danh sách các ID sản phẩm
+        val selectedProductIds = intent.getStringArrayListExtra("selectedProductIds") ?: emptyList<String>()
+
+        if (selectedProductIds.isNotEmpty()) {
+            loadSelectedProducts(selectedProductIds)
+        }
+    }
+
+    private fun loadSelectedProducts(productIds: List<String>) {
+        val firestore = FirebaseFirestore.getInstance()
+        val selectedProducts = mutableListOf<Product>()
+
+        CoroutineScope(Dispatchers.IO).launch {
+            for (productId in productIds) {
+                val result = firestore.collection("P")
+            }
+        }
     }
 }
